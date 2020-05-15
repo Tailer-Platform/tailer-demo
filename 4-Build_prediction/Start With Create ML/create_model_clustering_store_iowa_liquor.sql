44b0d22e5fe21a7148acf7d8bb27cea736da6eda
Romain Chaumais <romain.chaumais@fashiondata.io>
2020-05-15 15:08:20 +0200

    Delete Project References

--- 4-Build_prediction/Start With Create ML/create_model_clustering_store_iowa_liquor.sql
+++ 4-Build_prediction/Start With Create ML/create_model_clustering_store_iowa_liquor.sql
@@ -1,12 +1,12 @@
-###########################
-#  Train Clustering Model
-###########################
+###################################################
+#  Train Clustering Model in your Big Query Console
+###################################################
 
 CREATE OR REPLACE MODEL dlk_demo_iowa_liquor_bda_cluster.model_clustering_store_iowa_liquor
 OPTIONS(model_type='kmeans', num_clusters=5, standardize_features = true) AS
 
 SELECT * except( store_number, store_name, volume_sold_liters, sale_q1, sale_q2, sale_q3, sale_q4)
---FROM `fd-io-jarvis-demo-dlk.dlk_demo_iowa_liquor_bda_cluster.iowa_liquor_agg_store`
+--FROM `dlk_demo_iowa_liquor_bda_cluster.iowa_liquor_agg_store`
 FROM
 (
   
--- 4-Build_prediction/store_clustering.sql
+++ 4-Build_prediction/store_clustering.sql
@@ -6,6 +6,6 @@ SELECT * except(nearest_centroids_distance)
 FROM ML.PREDICT(MODEL dlk_demo_iowa_liquor_bda_cluster.model_clustering_store_iowa_liquor, 
 (
 SELECT * except(sale_q1, sale_q2, sale_q3, sale_q4)
-FROM `fd-io-jarvis-demo-dlk.dlk_demo_iowa_liquor_bda_cluster.iowa_liquor_agg_store`
+FROM `dlk_demo_iowa_liquor_bda_cluster.iowa_liquor_agg_store`
 
 ))
