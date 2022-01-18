2f571d0ed8202886ef4bfa5c95f3c2bc62c68fa0
Romain Chaumais <romain.chaumais@fashiondata.io>
2022-01-18 17:12:21 +0100

    Update demo Tailer Context

--- Demo_Configurations/06-tables-to-storage/sql/iowa_liquor_export_aggr_sales.sql
+++ Demo_Configurations/06-tables-to-storage/sql/iowa_liquor_export_aggr_sales.sql
@@ -3,15 +3,14 @@ SELECT date,
   EXTRACT(ISOYEAR FROM date) AS isoyear,
   EXTRACT(ISOWEEK FROM date) AS isoweek,
   sum(sale_dollars) as Total_Sales 
-FROM `fd-io-jarvis-demo-dlk.dlk_demo_iowa_liquor_pda.sales_details` 
+FROM dlk_demo_iowa_liquor_pda.sales_details
 WHERE date >= "2020-01-01"
 group by date, isoyear, isoweek
 order by date)
-​
 select min(date) as Week_date, 
   isoyear, 
   isoweek, 
   sum(Total_Sales) as Total_Sales_dollars
 from tmp
 group by isoyear,isoweek
-order by Week_date;
+order by Week_date
